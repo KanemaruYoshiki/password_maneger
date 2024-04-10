2812b33982c5b521543371ad100ea90cc899a238
KanemaruYoshiki <tomatomato.kryk@gmail.com>
2024-04-10 13:18:58 +0900

    "password_manegerの練習"

---
+++ password_maneger.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "パスワードマネージャーへようこそ!"
+
+echo "サービス名を入力してください："
+read service_name 
+
+echo "ユーザー名を入力してください："
+read user_name
+
+echo "パスワードを入力してください："
+read  password 
+
+echo "サービス名${service_name}: ユーザー名${user_name}: パスワード${password}" >> info.txt
+
+echo "Thank you!"
